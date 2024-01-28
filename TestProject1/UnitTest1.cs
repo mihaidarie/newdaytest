98484c6bb170211147e64a8024c81142dff5c7ee
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-28 16:38:22 +0200

    added initialization and method call from test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -5,11 +5,18 @@ namespace TestProject1
     [TestClass]
     public class UnitTest1
     {
-        private readonly DiamondPrinter testedObject;
+        private static DiamondPrinter testedObject;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            testedObject = new DiamondPrinter();
+        }
 
         [TestMethod]
         public void TestMethod1()
         {
+            testedObject.GeneratePrintableArray();
         }
     }
 }

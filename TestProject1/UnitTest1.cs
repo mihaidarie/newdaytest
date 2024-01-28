93e18efe43ef2853208a79e243577c76ce21d485
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-28 16:35:22 +0200

    added tested object

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -3,6 +3,8 @@ namespace TestProject1
     [TestClass]
     public class UnitTest1
     {
+        private readonly DiamondPrinter testedObject;
+
         [TestMethod]
         public void TestMethod1()
         {

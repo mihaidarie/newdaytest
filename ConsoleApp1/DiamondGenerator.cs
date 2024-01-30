c49498c59da2280d2a71216d19d56cb14e63d802
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-30 09:19:18 +0200

    returned empty model so test will pass

--- ConsoleApp1/DiamondGenerator.cs
+++ ConsoleApp1/DiamondGenerator.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     public class DiamondGenerator
     {
-        public void GenerateModel()
+        public IList<IList<char>> GenerateModel()
         {
-            
+            return new List<IList<char>>();
         }
     }
 }

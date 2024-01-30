cd35d05632d986608997f152c50a86f28fbe0531
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-30 14:19:30 +0200

    small refactor to reference the first item in the introduced alphabet

--- ConsoleApp1/DiamondGenerator.cs
+++ ConsoleApp1/DiamondGenerator.cs
@@ -1,7 +1,14 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections;
+
+namespace ConsoleApp1
 {
     public class DiamondGenerator
     {
+        private readonly HashSet<char> alphabet = new HashSet<char>
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'
+        };
+
         public IList<IList<char>> GenerateModel(char input)
         {
             if (input == default)
@@ -9,10 +16,12 @@
                 return new List<IList<char>> { new List<char> { } };
             }
 
-            if (input == 'A')
+            if (input == alphabet.ElementAt(0))
             {
                 return new List<IList<char>> { new List<char> { 'A' } };
             }
+
+
 
             return new List<IList<char>>();
         }

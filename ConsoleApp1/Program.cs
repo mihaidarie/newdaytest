62515d108a23667eb93393e1369e11942cc3b2ac
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-31 11:53:15 +0200

    removed all temp files

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("What's your favorite alphabet char?");
+
+var input = Console.ReadKey();
+
+var diamondRepresentation = new DiamondGenerator().GenerateModel(input.KeyChar);
+
+Console.WriteLine("\n");
+
+foreach (var item in diamondRepresentation)
+{
+    foreach (var item2 in item)
+    {
+        Console.Write(item2);
+    }
+
+    Console.WriteLine("\n");
+}

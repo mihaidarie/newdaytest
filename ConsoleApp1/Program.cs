8732d7c91b7698e148e0230b1f58540333a9ce06
Mihai Darie <mihai_darie2@yahoo.com>
2024-01-31 11:53:59 +0200

    removed the hello world

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp1;
 
-Console.WriteLine("Hello, World!");
-
 Console.WriteLine("What's your favorite alphabet char?");
 
 var input = Console.ReadKey();
